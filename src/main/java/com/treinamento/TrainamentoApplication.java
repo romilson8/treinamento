3c5e7079b7a0a8442f4fbb2cf87ecb1f6dacb2e8
carlos melo <carlossilvameloo@gmail.com>
2019-06-13 08:53:30 -0300

    first commit

---
+++ src/main/java/com/treinamento/TrainamentoApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainamentoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/treinamento/rest/Api.java
@@ -0,0 +1,16 @@
+package com.treinamento.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class Api {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String api() {
+		return "API treinamento Java EE";
+	}
+}
