7e782040f0b0fa749d7352aaef75cff75a6a36d6
jose.romilson.barros <jose.romilson.barros@accenture.com>
2019-07-29 14:41:47 -0300

    criacao e povoamento das entidades pessoa, carro, endereco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,23 +3,43 @@ VALUES
 ('API treinamento', 1);
 
 
---PESSOA
-INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf)
-VALUES
-(1,'Marcos Feliciano', 75.2, 1.75, 'desenvolvedor CL10', 'masculino', '1993-04-29', '04044664478' );
+--ENDERECO
 
-INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf)
+INSERT INTO ENDERECO (id, rua, cep, bairro, cidade)
 VALUES
-(2,'David Rodrigues', 60.2, 1.81, 'desenvolvedorCL11', 'masculino', '1998-05-13', '0406669997' );
+(1,'Viriato Correia', '51030510', 'Boa Viagem', 'Recife');
 
-INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf)
+INSERT INTO ENDERECO (id, rua, cep, bairro, cidade)
 VALUES
-(3,'Landerson Silva', 52.2, 1.66, 'desenvolvedorFront', 'masculino', '1999-11-30', '0406669997' );
+(2,'Monoel Borba', '51001130', 'Boa Vista', 'Recife');
 
-INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf)
+INSERT INTO ENDERECO (id, rua, cep, bairro, cidade)
 VALUES
-(4,'Myller Claudino', 75, 1.61, 'desenvolvedorCL10', 'masculino', '1991-11-03', '77799988855' );
+(3,'Machado de Assis', '50000510', 'Bairro Novo', 'Olinda');
 
+INSERT INTO ENDERECO (id, rua, cep, bairro, cidade)
+VALUES
+(4,'Encanta Moça', '52000350', 'Pina', 'Recife');
+
+
+--PESSOAS
+INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf, endereco_id)
+VALUES
+(1,'Marcos Feliciano', 75.2, 1.75, 'desenvolvedor CL10', 'masculino', '1993-04-29', '04044664478', 2);
+
+INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf, endereco_id)
+VALUES
+(2,'David Rodrigues', 60.2, 1.81, 'desenvolvedorCL11', 'masculino', '1998-05-13', '0406669997', 3);
+
+INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf, endereco_id)
+VALUES
+(3,'Landerson Silva', 52.2, 1.66, 'desenvolvedorFront', 'masculino', '1999-11-30', '0406669997',4);
+
+INSERT INTO PESSOA (id, nome, peso, altura, profissao, sexo, datanascimento, cpf, endereco_id)
+VALUES
+(4,'Myller Claudino', 75, 1.61, 'desenvolvedorCL10', 'masculino', '1991-11-03', '77799988855', 1);
+
+--CARROS
 INSERT INTO CARRO (id, modelo, ano, marca, pessoa_id)
 VALUES
 (1,'Coope SuperCharge', '2019-2020', 'Mini', 1);
@@ -31,3 +51,4 @@ VALUES
 INSERT INTO CARRO (id, modelo, ano, marca, pessoa_id)
 VALUES
 (3,'Compass', '2018-2019', 'Jeep', 3);
+
